c11630756e856a34e59c105fa6333cc80b5e7119
parsehex <1779937+parsehex@users.noreply.github.com>
2025-02-23 13:43:04 -0500

    update repo link

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -10,11 +10,10 @@ export default function Header(sketch: string, showLinks?: boolean): string {
   return `
 	<header>
     <h1>${spaceWords(sketch)} Sketch</h1>
-    ${
-      showLinks
-        ? `<a class="link" href="https://p5js.org/" target="_blank">P5.js Docs</a>
-		       <a class="link" href="https://github.com/StillScripts/p5-starter" target="_blank">View on GitHub</a>`
-        : ""
+    ${showLinks
+      ? `<a class="link" href="https://p5js.org/" target="_blank">P5.js Docs</a>
+		       <a class="link" href="https://github.com/parsehex/p5-apps" target="_blank">View on GitHub</a>`
+      : ""
     }
   </header>`;
 }
